487f9677762b974e718f12483fc867460174a3d3
sidhart25 <navneetnishchal1420@gmail.com>
2025-06-19 21:20:45 +0530

    error middleware added to the app

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express, { urlencoded } from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import errorMiddleware from "./middlewares/error.js";
 
 const app = express();
 
@@ -8,5 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/v1", user);
+
+app.use(errorMiddleware);
 
 export default app;
---
+++ server/middlewares/error.js
@@ -0,0 +1,8 @@
+export default errorMiddleware = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
+  res.statusCode(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
