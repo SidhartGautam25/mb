e61fe524ad0892a628488aecfc4fd0dbf367ab69
sidhartGautam25 <navneetnishchal1420@gmail.com>
2025-07-04 20:45:46 +0530

    hero improved and snapbharat is added to cors allowed origins

--- client/src/comp/hero/Hero.tsx
+++ client/src/comp/hero/Hero.tsx
@@ -4,9 +4,9 @@ import Right from "./Right";
 
 const Hero: React.FC = () => {
   return (
-    <section className="flex flex-col md:flex-row w-full max-w-7xl mx-auto my-8 bg-white">
+    <section className="flex flex-col md:flex-row w-full max-w-7xl mx-auto my-8 bg-white h-[450px]">
       <Left />
-      <div className="border-l border-gray-200"></div> {/* Thin gray border */}
+      <div className="border-l border-gray-200"></div>
       <Right />
     </section>
   );
