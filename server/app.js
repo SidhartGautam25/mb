90fe6bfe2e46978ff03726797749a0285565f4a5
sidhartGautam25 <navneetnishchal1420@gmail.com>
2025-07-04 20:18:45 +0530

    base url is added for axios instance and cors is added in backend

--- server/app.js
+++ server/app.js
@@ -4,12 +4,23 @@ import dotenv from "dotenv";
 import errorMiddleware from "./middlewares/error.js";
 import userRoute from "./routes/userRoutes.js";
 import productRoutes from "./routes/productRoute.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      
+    ],
+    credentials: true,
+  })
+);
 dotenv.config();
 
 app.use((req, res, next) => {
